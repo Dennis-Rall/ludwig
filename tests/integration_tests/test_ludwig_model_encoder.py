24e1931cb7721fe4c279bf1418083568769fc73b
Dennis Rall <dennis.rall@wogra.com>
2024-01-08 08:54:56 +0100

    test: load a model with a deleted transfer learning model

--- tests/integration_tests/test_ludwig_model_encoder.py
+++ tests/integration_tests/test_ludwig_model_encoder.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pathlib
+import shutil
 from typing import Any
 
 import pytest
@@ -12,7 +14,7 @@ from ludwig.models.ecd import ECD
 
 
 @pytest.mark.slow
-def test_ludwig_model_encoder(tmpdir: str):
+def test_ludwig_model_encoder(tmpdir: pathlib.Path):
     df = mnist.load()[::1000]
     config: dict[str, Any] = {
         "input_features": [
@@ -28,12 +30,14 @@ def test_ludwig_model_encoder(tmpdir: str):
     model = LudwigModel(config)
     model.train(df)
 
-    model.save(tmpdir)
+    save_path = str(tmpdir / "model")
+
+    model.save(str(save_path))
 
     transfer_config = config.copy()
     transfer_config["input_features"][0]["encoder"] = {
         "type": "ludwig-model",
-        "path": str(tmpdir),
+        "path": save_path,
         "input_feature": "image_path",
         "trainable": False,
     }
@@ -48,3 +52,15 @@ def test_ludwig_model_encoder(tmpdir: str):
     encoded_normal = model.model.encode(data)["image_path"][ENCODER_OUTPUT]
     encoded_transfer = transfer_model.model.encode(data)["image_path"][ENCODER_OUTPUT]
     assert torch.equal(encoded_normal, encoded_transfer)
+
+    # check loading of a ludwig model encoder
+    shutil.rmtree(save_path)
+    transfer_save_path = str(tmpdir / "transfer")
+    transfer_model.save(transfer_save_path)
+    loaded_model = LudwigModel.load(transfer_save_path)
+    assert isinstance(loaded_model.model, ECD)
+    tensor = tensor.to(loaded_model.model.device)
+    data["image_path"] = tensor
+    encoded_loaded = loaded_model.model.encode(data)["image_path"][ENCODER_OUTPUT]
+    encoded_normal = encoded_normal.to(loaded_model.model.device)
+    assert torch.allclose(encoded_normal, encoded_loaded, atol=1e-3)
